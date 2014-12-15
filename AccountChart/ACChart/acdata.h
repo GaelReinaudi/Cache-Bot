7f9b42eac6f01d725726ef8543a8bbcde255ddac
Gael Reinaudi <gael.reinaudi@gmail.com>
2014-12-14 21:58:49 -0500

    following this example to load the transaction list ("Level" in the example): https://qt-project.org/doc/qt-5-snapshot/qtcore-savegame-example.html

--- AccountChart/ACChart/acdata.h
+++ AccountChart/ACChart/acdata.h
@@ -3,6 +3,7 @@
 
 #include <QDateTime>
 #include <QJsonObject>
+#include <QJsonArray>
 
 class Transaction
 {
@@ -34,6 +35,38 @@ private:
 	double m_price = 0.0;
 	QDateTime m_time;
 	QString m_description;
+};
+
+class Transactions
+{
+public:
+	Transactions();
+
+	//! json in
+	void read(const QJsonObject &json)
+	{
+		m_trans.clear();
+		QJsonArray npcArray = json["transactions"].toArray();
+		for (int npcIndex = 0; npcIndex < npcArray.size(); ++npcIndex) {
+			QJsonObject npcObject = npcArray[npcIndex].toObject();
+			Transaction tra;
+			tra.read(npcObject);
+			m_trans.append(tra);
+		}
+	}
+	//! json out
+	void write(QJsonObject &json) const
+	{
+		QJsonArray npcArray;
+		foreach (const auto tra, m_trans) {
+			QJsonObject npcObject;
+			tra.write(npcObject);
+			npcArray.append(npcObject);
+		}
+		json["transactions"] = npcArray;
+	}
+private:
+	QList<Transaction> m_trans;
 };
 
 class ACData
