ef87a8b87ea6e523a34d39ffcb1d82d3b08ec213
Gael Reinaudi <gael.reinaudi@gmail.com>
2015-04-08 20:51:46 -0400

    fitness that slightly re-put the accent on recent transactions (bills without a miss)

--- evoCache/AccRegPrimits.cpp
+++ evoCache/AccRegPrimits.cpp
@@ -153,7 +153,7 @@ void FeatureMonthlyAmount::execute(void *outDatum, Puppy::Context &ioContext)
 		m_fitness += totalOneOverExpDist;
 		//m_fitness += qAbs(kindaLog(m_bundle.sumDollar())) * totalOneOverExpDist / m_bundle.count();
 		//m_fitness *= m_consecMonthBeforeMissed;
-		//m_fitness /= 2 + m_consecMissed;
+		m_fitness *= 1.0 + (1.0 / (1.0 + m_consecMissed));
 	}
 	output = m_fitness;
 
@@ -201,6 +201,6 @@ void FeatureMonthlyAmount::execute(void *outDatum, Puppy::Context &ioContext)
 			Transaction& t = m_bundle.trans(i);
 			emit evoSpinner()->sendMask(t.time_t(), t.amountDbl(), false);
 		}
-		qDebug() << targetTrans.count() << m_bundle.count();
+		//qDebug() << targetTrans.count() << m_bundle.count();
 	}
 }
