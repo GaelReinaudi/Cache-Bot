49d99ccdf30e1525ae4400277ac4ec340e7f9e77
Gael Reinaudi <gael.reinaudi@gmail.com>
2015-05-22 10:39:43 -0400

    trigger fabric call

--- expressjs/server.js
+++ expressjs/server.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app     = express();
 var port    =   process.env.PORT || 8081;
 var spawn = require('child_process').spawn;
+var _ = require('underscore'); // for some utility goodness
 
 // sample route
 app.get('/sample', function(req, res) {
-    var _ = require('underscore'); // for some utility goodness
     var deploySh = spawn('sh', [ 'test.sh' ], {
       cwd: '/home/ubuntu/Cache-Bot/expressjs',
       env:_.extend(process.env, { PATH: process.env.PATH + ':/usr/local/bin' })
@@ -16,17 +16,16 @@ app.get('/sample', function(req, res) {
 
 app.get('/cache-bot/extraCash/:user_id', function(req, res) {
 	console.log('POST to /cache-bot/extracash/:user_id')
-	if (req.params.user_id == null) {
-		var response = {
-			"error" : "Cannot find either req.params.user_id in POST URL"
-		}
-		res.send(200, response)
-	} else {
-		var response = {
-			"success" : "Successfully parsed extra cash for user."
-		}
-		res.send(200, response)
+	var response = {
+		"success" : "Successfully parsed extra cash for user: " + req.params.user_id
 	}
+	res.send(200, response)
+
+    var deploySh = spawn('fab', [ '-l' ], {
+      cwd: '/home/ubuntu/Cache-Bot/expressjs',
+      env:_.extend(process.env, { PATH: process.env.PATH + ':/usr/local/bin' })
+    });
+
 });
 
 // Start the server
