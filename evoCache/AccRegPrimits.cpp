7ae5c6c0055f85a44b923fd1056fd8b262a2f39f
Gael Reinaudi <gael.reinaudi@gmail.com>
2015-06-09 10:35:20 -0400

    typo

--- evoCache/AccRegPrimits.cpp
+++ evoCache/AccRegPrimits.cpp
@@ -174,7 +174,7 @@ void FeatureMonthlyAmount::execute(void *outDatum, Puppy::Context &ioContext)
 		}
 
 		if(m_billProba > 1.0) {
-			QJsonArray features = (*ioContext.m_summaryJsonObj)["Features"].toArray();
+			QJsonArray features = (*ioContext.m_summaryJsonObj)["features"].toArray();
 			features.append(toJson(ioContext));
 			ioContext.m_summaryJsonObj->insert("features", features);
 		}
