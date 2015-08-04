034dbca19830fdc4e9156ace80064a844489cea1
Gael Reinaudi <gael.reinaudi@gmail.com>
2015-08-04 10:45:50 -0400

    continuing moving args to the structure. about to fix the periodic amount feature.

--- evoCache/puppy/featureStatDistrib.h
+++ evoCache/puppy/featureStatDistrib.h
@@ -8,19 +8,20 @@ class OracleStatDistrib : public Oracle
 public:
 
 protected:
-//	QJsonObject toJson(Puppy::Context& ioContext) override {
-//		QJsonObject retObj = AccountFeature::toJson(ioContext);
-//		retObj.insert("hash", m_hash);
-//		retObj.insert("days", m_daysBundle);
-//		retObj.insert("proba", m_dayProba);
-//		retObj.insert("avgAmn", m_bundle.count() ? m_bundle.sumDollar() / m_bundle.count() : 0.0);
-//		return retObj;
-//	}
 	QVector<Transaction> revelation(QDate upToDate) override;
 
 private:
 	struct Args
 	{
+		void intoJson(QJsonObject& o_retObj) {
+			o_retObj.insert("hash", m_hash);
+			o_retObj.insert("days", m_daysBundle);
+			o_retObj.insert("proba", m_dayProba);
+			o_retObj.insert("avgAmn", m_bundle.count() ? m_bundle.sumDollar() / m_bundle.count() : 0.0);
+			o_retObj.insert("labels", QJsonArray::fromStringList(m_bundle.uniqueNames()));
+			o_retObj.insert("tot$", m_bundle.sumDollar());
+			o_retObj.insert("numBund", m_bundle.count());
+		}
 		TransactionBundle m_bundle;
 		int m_hash = 0;
 		double m_dayProba = 0.0;
@@ -42,13 +43,7 @@ protected:
 	void getArgs(Puppy::Context &ioContext) override;
 	QJsonObject toJson(Puppy::Context& ioContext) override {
 		QJsonObject retObj = AccountFeature::toJson(ioContext);
-		retObj.insert("hash", m_localStaticArgs.m_hash);
-		retObj.insert("days", m_localStaticArgs.m_daysBundle);
-		retObj.insert("proba", m_localStaticArgs.m_dayProba);
-		retObj.insert("avgAmn", m_localStaticArgs.m_bundle.count() ? m_localStaticArgs.m_bundle.sumDollar() / m_localStaticArgs.m_bundle.count() : 0.0);
-		retObj.insert("labels", QJsonArray::fromStringList(m_localStaticArgs.m_bundle.uniqueNames()));
-		retObj.insert("tot$", m_localStaticArgs.m_bundle.sumDollar());
-		retObj.insert("numBund", m_localStaticArgs.m_bundle.count());
+		m_localStaticArgs.intoJson(retObj);
 		return retObj;
 	}
 	void execute(void* outDatum, Puppy::Context& ioContext) override;
