fabfc16f1279e7cc59685b365ab8aabaacc276bf
Gael Reinaudi <gael.reinaudi@gmail.com>
2015-08-25 15:25:55 -0400

    manages post on EC2

--- expressjs/server.js
+++ expressjs/server.js
@@ -11,7 +11,7 @@ app.get('/cache-bot/fullAnalysis/:user_id', function(req, res) {
     var runFull = child_process.execFile(
         '../../expressjs/fullAnalysis.sh', [req.params.user_id], {
             cwd: '/home/ubuntu/Cache-Bot/bin/release',
-            timeout: 600000 },
+            timeout: 0 },
         function(err, stdout, stderr) {
             console.log(err);
             console.log(stdout);
@@ -24,6 +24,42 @@ app.get('/cache-bot/fullAnalysis/:user_id', function(req, res) {
 });
 
 app.get('/cache-bot/extraCash/:user_id', function(req, res) {
+    console.log('POST to /cache-bot/extraCash/:user_id('+req.params.user_id+')')
+
+    var runExtra = child_process.execFile(
+        '../../expressjs/extraCash.sh', [req.params.user_id], {
+            cwd: '/home/ubuntu/Cache-Bot/bin/release',
+            timeout: 30000 },
+        function(err, stdout, stderr) {
+            console.log(err);
+            console.log(stdout);
+            console.log(stderr);
+            var response = "ran extraCache for user: " + req.params.user_id+'\n'
+            + stdout + '\n' + stderr;
+            res.status(200).send(response)
+        }
+    );
+});
+
+app.post('/cache-bot/fullAnalysis', function(req, res) {
+    console.log('POST to /cache-bot/fullAnalysis/:user_id')
+
+    var runFull = child_process.execFile(
+        '../../expressjs/fullAnalysis.sh', [req.params.user_id], {
+            cwd: '/home/ubuntu/Cache-Bot/bin/release',
+            timeout: 0 },
+        function(err, stdout, stderr) {
+            console.log(err);
+            console.log(stdout);
+            console.log(stderr);
+            var response = "ran fullAnalysis for user: " + req.params.user_id+'\n'
+            + stdout + '\n' + stderr;
+            res.status(200).send(response)
+        }
+    );
+});
+
+app.post('/cache-bot/extraCash', function(req, res) {
     console.log('POST to /cache-bot/extraCash/:user_id('+req.params.user_id+')')
 
     var runExtra = child_process.execFile(
