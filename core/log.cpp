f0015ce322ae377ccaa0ca0059082c7a75a1520e
Gael Reinaudi <gael.reinaudi@gmail.com>
2015-09-02 16:10:42 -0400

    async logging

--- core/log.cpp
+++ core/log.cpp
@@ -24,7 +24,8 @@ void CORESHARED_EXPORT logger::setupSpdLog(QString logFileName)
 	try
 	{
 		//Create console, multithreaded logger
-		s_pLog->m_fileLogger = spdlog::create<spdlog::sinks::rotating_file_sink_mt>("file", logFileName.toStdString(), "log", 1024 * 1024 * 20, 1);
+		spdlog::set_async_mode(1024);
+		s_pLog->m_fileLogger = spdlog::create<spdlog::sinks::rotating_file_sink_st>("file", logFileName.toStdString(), "log", 1024 * 1024 * 20, 1, true);
 		s_pLog->m_fileLogger->set_pattern("[%l] %v");
 		s_pLog->m_fileLogger->set_level(spdlog::level::info);
 		s_pLog->m_fileLogger->warn() << "********** NEW SESSION ********** "
