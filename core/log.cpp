465a1021025aea810e054e053288754ee5a7bbfe
Gael Reinaudi <gael.reinaudi@gmail.com>
2015-09-04 09:39:28 -0400

    try to fix crash on linux

--- core/log.cpp
+++ core/log.cpp
@@ -24,12 +24,12 @@ void CORESHARED_EXPORT logger::setupSpdLog(QString logFileName)
 	try
 	{
 		//Create console, multithreaded logger
-		spdlog::set_async_mode(1024);
+//		spdlog::set_async_mode(1024);
 		s_pLog->m_fileLogger = spdlog::create<spdlog::sinks::rotating_file_sink_st>("file", logFileName.toStdString(), "log", 1024 * 1024 * 20, 1, true);
-		s_pLog->m_fileLogger->set_pattern("[%l] %v");
-		s_pLog->m_fileLogger->set_level(spdlog::level::info);
-		s_pLog->m_fileLogger->warn() << "********** NEW SESSION ********** "
-									 << QDateTime::currentDateTime().toString("HH:mm:ss.zzz");
+//		s_pLog->m_fileLogger->set_pattern("[%l] %v");
+//		s_pLog->m_fileLogger->set_level(spdlog::level::info);
+//		s_pLog->m_fileLogger->warn() << "********** NEW SESSION ********** "
+//									 << QDateTime::currentDateTime().toString("HH:mm:ss.zzz");
 	}
 	catch (const spdlog::spdlog_ex& ex)
 	{
