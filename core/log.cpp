3383fd95fe59fbd64b194b7ac6e76e82c2fd23a5
Gael Reinaudi <gael.reinaudi@gmail.com>
2015-09-04 11:15:59 -0400

    trying to fix

--- core/log.cpp
+++ core/log.cpp
@@ -24,9 +24,9 @@ void CORESHARED_EXPORT logger::setupSpdLog(QString logFileName)
 	try
 	{
 		//Create console, multithreaded logger
-//		spdlog::set_async_mode(1024);
-//		s_pLog->m_fileLogger = spdlog::create<spdlog::sinks::rotating_file_sink_st>("file", logFileName.toStdString(), "log", 1024 * 1024 * 20, 1, true);
-//		s_pLog->m_fileLogger->set_pattern("[%l] %v");
+		spdlog::set_async_mode(1024);
+		s_pLog->m_fileLogger = spdlog::create<spdlog::sinks::rotating_file_sink_st>("file", logFileName.toStdString(), "log", 1024 * 1024 * 20, 1, true);
+		s_pLog->m_fileLogger->set_pattern("[%l] %v");
 //		s_pLog->m_fileLogger->set_level(spdlog::level::info);
 //		s_pLog->m_fileLogger->warn() << "********** NEW SESSION ********** "
 //									 << QDateTime::currentDateTime().toString("HH:mm:ss.zzz");
