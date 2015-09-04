e727a4c1b97b2b944762b932f28d621c76510b0b
Gael Reinaudi <gael.reinaudi@gmail.com>
2015-09-04 11:39:08 -0400

    console worked. trying more

--- core/log.cpp
+++ core/log.cpp
@@ -25,7 +25,7 @@ void CORESHARED_EXPORT logger::setupSpdLog(QString logFileName)
 	{
 		//Create console, multithreaded logger
 //		spdlog::set_async_mode(1024);
-		s_pLog->m_fileLogger = spdlog::stdout_logger_mt("console");
+		s_pLog->m_fileLogger = spdlog::rotating_logger_mt("file_logger", "myfilename", 1024 * 1024 * 5, 3);
 //		s_pLog->m_fileLogger = spdlog::create<spdlog::sinks::rotating_file_sink_mt>("file", logFileName.toStdString(), "log", 1024 * 1024 * 20, 1, true);
 		s_pLog->m_fileLogger->set_pattern("[%l] %v");
 		s_pLog->m_fileLogger->set_level(spdlog::level::info);
