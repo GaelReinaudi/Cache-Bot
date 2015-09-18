6a48a8e5a2bc2259ecfc6c044fcbc882d2c0ad60
Gael Reinaudi <gael.reinaudi@gmail.com>
2015-09-18 13:00:40 -0400

    changing the priceWindow range to be +-0.2 in kla

--- evoCache/puppy/featurePriceWindow.h
+++ evoCache/puppy/featurePriceWindow.h
@@ -32,11 +32,12 @@ public:
 protected:
 	bool passFilter(quint64 dist, const Transaction& trans) const override {
 		Q_UNUSED(dist);
-		return (trans.kla() >= 1.04 * m_localStaticArgs.m_kla
-				&& trans.kla() <= 0.95 * m_localStaticArgs.m_kla)
-				||
-				(trans.kla() <= 1.14 * m_localStaticArgs.m_kla
-				&& trans.kla() >= 0.85 * m_localStaticArgs.m_kla);
+		if (m_localStaticArgs.m_kla < 0)
+		return trans.kla() >= m_localStaticArgs.m_kla + 0.2
+				&& trans.kla() <= m_localStaticArgs.m_kla - 0.2;
+		else
+			return trans.kla() <= m_localStaticArgs.m_kla + 0.2
+				&& trans.kla() >= m_localStaticArgs.m_kla - 0.2;
 	}
 	int minTransactionForBundle() const override { return 2; }
 
