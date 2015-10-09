6457c7e2c510f5b5b93d702a9d3f862f6f92b733
Gael Reinaudi <gael.reinaudi@gmail.com>
2015-10-08 22:53:31 -0400

    ammend

--- evoCache/main.cpp
+++ evoCache/main.cpp
@@ -8,10 +8,10 @@ int main(int argc, char *argv[])
 		qDebug() << "needs an argument";
 		return 0;
 	}
-	QString jsonStr = QString(argv[1]);
+	QString jsonStr = QString(argv[1]).remove("'");
 	QJsonDocument jsonDoc(QJsonDocument::fromJson(jsonStr.toUtf8()));
 	const QJsonObject& jsonObj = jsonDoc.object();
-	qDebug() << jsonStr << jsonObj;
+	qDebug() << endl << jsonStr << endl << jsonObj;
 	Evolver w(jsonObj["user_id"].toString().trimmed());
 
 	return a.exec();
