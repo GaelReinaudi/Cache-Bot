f71be7cf98079dab0d7a28b4a399b16151348638
Gael Reinaudi <gael.reinaudi@gmail.com>
2015-10-08 21:57:56 -0400

    evoCache feeds from json as first parameter

--- evoCache/main.cpp
+++ evoCache/main.cpp
@@ -4,11 +4,15 @@
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
-	QString jsonFileOrUser = "../../data/adelineGaelTransactions.json";
-	if(argc > 1) {
-		jsonFileOrUser = argv[1];
+	if(argc < 2) {
+		qDebug() << "needs an argument";
+		return 0;
 	}
-	Evolver w(jsonFileOrUser);
+	QString jsonStr = QString(argv[1]);
+	QJsonDocument jsonDoc(QJsonDocument::fromJson(jsonStr.toUtf8()));
+	const QJsonObject& jsonObj = jsonDoc.object();
+	qDebug() << jsonStr << jsonObj;
+	Evolver w(jsonObj["user_id"].toString().trimmed());
 
 	return a.exec();
 }
--- expressjs/evo.sh
+++ expressjs/evo.sh
@@ -3,5 +3,5 @@ pwd
 echo $1
 echo $2
 export LD_LIBRARY_PATH=$(pwd):$LD_LIBRARY_PATH
-./evoCache $1 $2
+./evoCache $2
 
