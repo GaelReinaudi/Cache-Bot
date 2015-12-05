3d8b674afdf5736229b2c47c01c3ad0efcb04d20
Gael Reinaudi <gael.reinaudi@gmail.com>
2015-12-05 17:17:48 -0500

    avgAmnt is now the "average Smart"

--- evoCache/puppy/featurePeriodicAmount.h
+++ evoCache/puppy/featurePeriodicAmount.h
@@ -11,8 +11,8 @@ public:
 	{}
     QJsonObject toJson() const override {
         QJsonObject ret = Oracle::toJson();
-		ret["approxAmnt"] = toSignifDigit_2(m_args.m_bundle.averageAmount());
-		ret["avgAmnt"] = m_args.m_bundle.averageAmount();
+		ret["approxAmnt"] = toSignifDigit_2(m_args.m_bundle.avgSmart());
+		ret["avgAmnt"] = m_args.m_bundle.avgSmart();
 		ret["day1"] = (m_args.m_dayOfMonth + 31) % 31;
 		ret["day2"] = (m_args.m_dayOfMonth2 + 31) % 31;
         ret["cons-missed"] = m_args.m_consecMissed;
