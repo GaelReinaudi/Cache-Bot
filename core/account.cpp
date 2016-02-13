31701bb761742f95176713b2c041c2733a8f25f6
Gael Reinaudi <gael.reinaudi@gmail.com>
2016-02-13 16:53:34 -0500

    moneymarket account labeled as saving

--- core/account.cpp
+++ core/account.cpp
@@ -67,6 +67,10 @@ void Account::loadJsonData(QJsonObject json)
 		m_type = Type::Credit;
 		NOTICE() << "Credit";
 	}
+	if (accountSubType.contains("moneymarket", Qt::CaseInsensitive)) {
+		m_type = Type::Saving;
+		NOTICE() << "Saving";
+	}
 	if (m_type == Type::Unknown) {
 		ERR() << "Unknown account type. metaName: " << accountMetaName << " . type: " << accountType << " . subtype: " << accountSubType;
 		m_type = Type::Checking;
