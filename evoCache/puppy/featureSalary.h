755915b2a42f1f234ea67c704bb449a3faef7c52
Gael Reinaudi <gael.reinaudi@gmail.com>
2016-03-04 21:50:55 -0500

    back to more narrow salary

--- evoCache/puppy/featureSalary.h
+++ evoCache/puppy/featureSalary.h
@@ -41,7 +41,7 @@ protected:
 	qint64 distance(const Transaction *targ, const Transaction *trans) override {
 		// if both are positive
 		if (targ->amount() > 0 && trans->amount() > 0) {
-			return targ->distanceWeighted<16*2, 512, 2*4>(*trans);
+			return targ->distanceWeighted<16*2, 512/2, 2*4>(*trans);
 		}
 		return 1<<20;
 	}
